975d5ae143dc0cb6807d241c7f9b51e6f2e1d50d
Renjish-k-s <146869658+Renjish-k-s@users.noreply.github.com>
2023-12-13 11:16:21 +0530

    7 th program added

---
+++ wpl_lab_works/5-10/7.php
@@ -0,0 +1,51 @@
+<?php
+
+$con= new mysqli("localhost","root","","mca_dept");
+
+
+if(!$con->connect_error)
+{
+        print("connection sucess");
+}
+else
+{
+    print("connectio_error:".$con->connect_error);
+}
+
+$sql="CREATE TABLE IF NOT EXISTS TEST (ID INT(10) PRIMARY KEY, SNAME VARCHAR(20))";
+
+if($con->query($sql))
+{
+
+    echo"<script>alert('table created')</script>";
+}
+else
+{
+    print("connection error:".$con->connect_error);
+}
+
+// $sql="INSERT INTO TEST (ID,SNAME) VALUES ('1','hari')";
+// if ($con->query($sql)) 
+// {
+//     echo"<script>alert('inserted')</script>";
+// }
+// else
+// {
+//     print("connection error".$con->connect_error);
+// }
+
+// $sql="UPDATE TEST SET SNAME='ROY' WHERE ID='1'";
+// if ($con->query($sql)) 
+// {
+
+//     echo"<script>alert('UPDATED')</script>";    
+// }
+// else {
+//     print("connection error".$con->connect_error);
+// }
+$sql="ALTER TABLE TEST ADD AGE VARCHAR(20)";
+if ($con->query($sql)) 
+{
+    echo"<script>alert('ALTERED THE TABLE')</script>";
+}
+?>
